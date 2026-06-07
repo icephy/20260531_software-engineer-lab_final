14c9ddee90bc794d8be988616e900f5f5e8ff0a4
Murphy <yppice.adc@gmail.com>
2026-06-07 23:25:29 +0800

    2026-軟體工程師體驗營-最終任務-個人品牌網站

---
+++ index.js
@@ -0,0 +1,8 @@
+const menuBtn = document.querySelector('.menu-btn')
+const navList = document.querySelector('.nav-list')
+
+menuBtn.addEventListener('click', function () {
+    navList.classList.toggle('active');
+    document.body.classList.toggle('menu-open')
+})
+ 
